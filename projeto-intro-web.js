87964be1a986fdf6f49a720247b376d5dd38b93f
Lucas Breia <lucas@MacBook-Air-de-Lucas.local>
2022-10-29 10:16:01 -0300

    Projeto em andamento

---
+++ projeto-intro-web.js
@@ -0,0 +1,8 @@
+//começando o projeto introduzindo Arrays.
+
+const cursos = ["HTML e CSS", "JavaScript", "APIs REST"];
+const turmas = ["HC1", "JS1", "JS2", "REST1", "REST2"];
+const estudantes = ["João", "Ana", "Bruna"];
+
+
+
