f07f28fded5d4e9d802ac0f01d4b5f9f932facd9
Lucas Breia <lucas@MacBook-Air-de-Lucas.local>
2022-10-30 14:19:38 -0300

    Projeto em andamento

--- projeto-intro-web.js
+++ projeto-intro-web.js
@@ -1,8 +1,86 @@
 //começando o projeto introduzindo Arrays.
 
-const cursos = ["HTML e CSS", "JavaScript", "APIs REST"];
-const turmas = ["HC1", "JS1", "JS2", "REST1", "REST2"];
-const estudantes = ["João", "Ana", "Bruna"];
+// const cursos = ["HTML e CSS", "JavaScript", "APIs REST"];
+// const turmas = ["HC1", "JS1", "JS2", "REST1", "REST2"];
+// const estudantes = ["João", "Ana", "Bruna"];
+
+//Array Objeto dos cursos
+
+const cursos = [
+{
+    //Primeiro curso contendo só uma turma.
+    nome: "HTML e CSS",
+    descricao: "Aprenda a cria websites com HTML e CSS!",
+    duracao: "6 meses",
+    valor: 1000
+},
+{
+    //Segundo curso contendo duas turmas.
+    nome: "JavaScript",
+    descricao: "Aprenda a linguagem de progamação mais utilizada no mundo!",
+    duracao: "6 meses",
+    valor: 1500
+},
+{
+    //Terceiro curso contendo duas turmas.
+    nome: "APIs REST",
+    descricao: "Quer aprender arquitetura de software? temos um curso para você!",
+    duracao: "6 meses",
+    valor: 1500
+}];
+
+const turmas = [
+{
+    //Turma HTML e CSS.
+    nome: "Berners",
+    curso: "HTML e CSS",
+    inicio: "22/08/2022",
+    termino: "22/02/2023",
+    numalunos: 10,
+    periodo: "6 meses",
+    conclusao: false
+},
+{
+    //Primeira turma JavaScript.
+    nome: "Lee",
+    curso: "JavaScript",
+    inicio: "22/08/2022",
+    termino: "22/02/2023",
+    numalunos: 10,
+    periodo: "6 meses",
+    conclusao: false
+},
+{
+    //Segunda turma JavaScript.
+    nome: "Eich",
+    curso: "JavaScript",
+    inicio: "22/02/2022",
+    termino: "22/08/2022",
+    numalunos: 10,
+    periodo: "6 meses",
+    conclusao: true
+},
+{
+    //Primeira turma API's REST.
+    nome: "Rossun",
+    curso: "APIs REST",
+    inicio: "22/12/2022",
+    termino: "22/05/2023",
+    numalunos: 10,
+    periodo: "6 meses",
+    conclusao: false
+},
+{
+    //Primeira turma HTML e CSS.
+    nome: "King",
+    curso: "APIs REST",
+    inicio: "22/01/2022",
+    termino: "22/06/2023",
+    numalunos: 10,
+    periodo: "6 meses",
+    conclusao: false
+}];
+
 
 
 
