c386cc591fc9ea6ecdafd73658f3258e360c2835
Cristian Viery <viery.guimaraes01@gmail.com>
2022-11-09 18:25:28 -0300

    parte array do projeto

---
+++ script.js
@@ -0,0 +1,7 @@
+const cursos = ["HTML e CSS", "JavaScript", "APIs REST"];
+const turmas = ["HC1", "JS1", "JS2", "REST1", "REST2"];
+const estudantes = ["Cristian", "Emerson", "Matheus", "Pedro"];
+
+console.log(cursos)
+console.log(turmas)
+console.log(estudantes)
